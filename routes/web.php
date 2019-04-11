3c090508aa8d76af54c45592cb43e861b6c82b73
EC2 Default User <ec2-user@ip-172-31-16-11.ap-southeast-1.compute.internal>
2019-04-11 02:52:50 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/holiday','CalendarController@getHoliday');
+Route::post('/holiday','CalendarController@postHoliday');
